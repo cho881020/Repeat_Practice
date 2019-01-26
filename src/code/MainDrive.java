381887d4c83bae29f7349da835e4db223dc5f0ca
조경진 <kj_cho@idealidea.co.kr>
2019-01-26 11:02:02 +0900

    1번 문제 출제

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -3,7 +3,17 @@ package code;
 public class MainDrive {
 
 	public static void main(String[] args) {
-		System.out.println("Git �׽�Ʈ");
+		
+//		1. 12345 를 5번 반복.
+		
+//		12345
+//		12345
+//		12345
+//		12345
+//		12345
+		
+		
+		
 	}
 	
 }
